924026ab295bda9f310fe686498472868badbb60
HuaMeng <dreamanne15@gmail.com>
2020-04-15 22:34:06 +0800

    Add GenerateInvariant

---
+++ GenerateInvariant/CalcHyperplane.cpp
@@ -0,0 +1,86 @@
+#include<algorithm>
+#include<fstream>
+#include<iostream>
+#include<iterator>
+#include<regex>
+#include<string>
+using namespace std;
+
+vector<string> Split(const string& in, const string& delim) {
+    regex re{ delim };
+    return vector<string> {
+        sregex_token_iterator(in.begin(), in.end(), re, -1),
+            sregex_token_iterator()
+    };
+}
+
+int main(int argc, char** argv){
+    if(argc < 3) {
+        cerr << "CalcHyperplane.cpp needs more paramters" << endl;
+        cerr << "./CalcHyperplane input.model outputname" << endl;
+        exit(-1);
+    }
+
+    ifstream svFile;
+    ofstream outputFile;
+    svFile.open(argv[1], ios::out | ios::in );
+    outputFile.open(argv[2], ios::out);
+    if(!svFile) {
+        cerr << "Can't open " << argv[1] << endl;
+        exit(-1);
+    }
+    if(!outputFile) {
+        cerr << "Can't open " << argv[2] << endl;
+        exit(-1);
+    }
+
+    string line;
+    // if read SV, support vector is after "SV"
+    bool isSV = false;
+    // the first parameter of support vector
+    // is coefficient
+    vector<double> coefficient;
+    // the remain part of support vector is alpha
+    vector<vector<int>> alpha;
+
+    while(getline(svFile, line)){
+        vector<string> res = Split(line, " ");
+        if(res[0] == "rho") {
+            outputFile << res[1] << endl;;
+        }
+        else if(res[0] == "SV") {
+            isSV = true;
+            continue;
+        }
+        if(isSV){
+            coefficient.push_back(stod(res[0]));
+            vector<int> temp;
+            for(int i = 1;i < res.size();i++){
+                vector<string> alphaItem = Split(res[i], ":");
+                temp.push_back(stod(alphaItem[1]));
+            }
+            alpha.push_back(temp);
+        }
+    }
+
+    if(alpha.size() != coefficient.size()) {
+        cerr << "coefficient size is not equal to alpha size" << endl;
+        exit(-1);
+    }
+    int size = alpha[0].size();
+    for(int i = 0;i < size;i++){
+        double result = 0;
+        cout << i << " : ";
+        for(int j = 0;j < coefficient.size();j++){
+            cout << coefficient[j] << "*" << alpha[j][i]
+                << "=" << coefficient[j] * alpha[j][i] << " ";
+            result += coefficient[j] * alpha[j][i];
+        }
+        cout << endl;
+        outputFile << result << endl;
+    }
+
+    svFile.close();
+    outputFile.close();
+    return 0;
+}
---
+++ GenerateInvariant/PredictNode.cpp
@@ -0,0 +1,83 @@
+#include<algorithm>
+#include<cmath>
+#include<ctime>
+#include<fstream>
+#include<iostream>
+#include<iterator>
+#include<random>
+#include<regex>
+#include<string>
+#define DEBUG
+using namespace std;
+
+void GenerateSample(const vector<double> &w, double b, ofstream &outputFile) {
+    int size = w.size();
+    vector<int> result;
+    for(int i = 0;i < size - 1;i++) {
+        int temp = (rand() % 201) - 100;
+        cout << temp << endl;
+        result.push_back(temp);
+    }
+
+    double sum = 0;
+    for(int i = 0;i < size - 1;i++){
+        sum += w[i] * result[i];
+    }
+    double x_n = (-b - sum) / w[size - 1];
+    int x_n_int = (int)x_n;
+    if(x_n - x_n_int < 0.5) {
+        result.push_back(x_n_int);
+    }
+    else {
+        result.push_back(x_n_int + 1);
+    }
+
+    for(int i = 0;i < size;i++){
+       outputFile << result[i] << " "; 
+    }
+    outputFile << endl;
+}
+
+int main(int argc, char** argv){
+    if(argc < 3) {
+        cerr << "PredictNode.cpp needs more paramters" << endl;
+        cerr << "./PredictNode input.model.parameter outputname" << endl;
+        exit(-1);
+    }
+
+    ifstream paramFile;
+    ofstream outputFile;
+    paramFile.open(argv[1], ios::out | ios::in );
+    outputFile.open(argv[2], ios::out);
+    if(!paramFile) {
+        cerr << "Can't open " << argv[1] << endl;
+        exit(-1);
+    }
+    if(!outputFile) {
+        cerr << "Can't open " << argv[2] << endl;
+        exit(-1);
+    }
+
+    string line;
+
+    getline(paramFile, line);
+    // w^T x + b = 0
+    double b = stod(line);
+
+    srand((int)time(0));
+
+    vector<double> w;
+    while(getline(paramFile, line)){
+        w.push_back(stod(line));
+    }
+
+    if(w.size() == 1) GenerateSample(w, b, outputFile);
+    else {
+    for(int i = 0;i < 5;i++)
+        GenerateSample(w, b, outputFile);
+    }
+
+    paramFile.close();
+    outputFile.close();
+    return 0;
+}
